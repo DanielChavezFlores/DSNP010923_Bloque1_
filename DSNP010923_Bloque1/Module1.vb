edfaa02efd871be691fe2d8ce4fac0343088a4de
Daniel Chavez <dsnp010923@ugb.edu.sv>
2023-09-22 17:27:59 -0600

    Agregar archivos de proyecto.

---
+++ DSNP010923_Bloque1/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main(Args As String())
+
+        Dim cadena1 As String.
+       
+       
+
+
+
+        Console.WriteLine("Ingrese la edad")
+
+        cadena1 = Console.ReadLine()
+        If (cadena1 < 0 Or cadena1 > 100) Then
+            Console.WriteLine()
+
+        End If
+        Console.WriteLine("Hoy si va a funcionar")
+        Console.ReadLine()
+    End Sub
+
+End Module
